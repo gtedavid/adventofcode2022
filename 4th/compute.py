f3eb8cf1c31c99cc90601934a17093d1e7b9fcbc
gtedavid <david.gorgette@gmail.com>
2022-12-04 20:09:09 +0000

    Reducing variables in part 1 for 4th day

--- 4th/compute.py
+++ 4th/compute.py
@@ -5,12 +5,11 @@ def part1(path_in):
     with open(path_in, "r") as input_file:
         fully_contained = 0
         for line in input_file.readlines():
-            part1=line.split(",")[0];
-            part2=line.split(",")[1];
-            part1_1=int(part1.split("-")[0]);
-            part1_2=int(part1.split("-")[1]);
-            part2_1=int(part2.split("-")[0]);
-            part2_2=int(part2.split("-")[1]);
+            part1_1=int(line.split(",")[0].split("-")[0]);
+            part1_2=int(line.split(",")[0].split("-")[1]);
+            part2_1=int(line.split(",")[1].split("-")[0]);
+            part2_2=int(line.split(",")[1].split("-")[1]);
+
             if (part1_1 <= part2_1 and part1_2 >= part2_2) or (part2_1 <= part1_1 and part2_2 >= part1_2):
                 fully_contained += 1
     return fully_contained
